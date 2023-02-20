5181d1ac51adc6dbea68dd56e039c9e543019270
Ahneb <benjaminha1996@gmail.com>
2023-02-20 01:39:53 -0800

    set up columns for the models and added in references between each model set up index page to also reference models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -16,5 +35,8 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
+
+ProductTag.belongsTo(Product, {foreignKey: 'product_id'});
+ProductTag.belongsTo(Tag, {foreignKey: 'tag_id'});
 
 module.exports = ProductTag;
